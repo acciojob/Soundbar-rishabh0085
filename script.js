0e7d4ee6e0c905adee926956fadd575d13de18db
Rishabh Singh <52191143+rishabh0085@users.noreply.github.com>
2024-04-05 23:59:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+function playSound(soundFile) {
+  var audio = document.getElementById('audio');
+  audio.src = 'sounds/' + soundFile;
+  audio.play();
+}
+
+function stopSound() {
+  var audio = document.getElementById('audio');
+  audio.pause();
+  audio.currentTime = 0;
+}
